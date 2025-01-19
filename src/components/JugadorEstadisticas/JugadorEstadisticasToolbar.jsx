ba6f4ecbd227274df4ad4f7bc7f36a741c137ea7
Facundo Zapata <facundozapata41@gmail.com>
2025-01-19 00:48:01 -0300

    Conección con backend y BD

--- src/components/JugadorEstadisticas/JugadorEstadisticasToolbar.jsx
+++ src/components/JugadorEstadisticas/JugadorEstadisticasToolbar.jsx
@@ -1,11 +1,29 @@
 /* eslint-disable react/prop-types */
 import { getEdadActual } from "../../Utils/DateUtils";
+import { useEffect, useState } from "react";
 import './JugadorEstadisticasToolbar.css'
 import soccerPlayer from '../../assets/soccer-player.png'
 
-const JugadorEstadisticasToolbar = ({ nombreJugador = "Facundo", fechaNac="1998-07-21" }) => {
+const URL = 'https://localhost:44330/api/Jugador/5';
+
+const JugadorEstadisticasToolbar = () => {
     const Goles = 52;
     const Rendimiento = 6.4;
+    const initialPersonalData = {
+        nombre: ''
+        ,apellido: ''
+        ,fechaNacimiento: ''
+    }
+    const [personalData, setPersonalData] = useState(initialPersonalData);
+    
+    useEffect(() => {
+        fetch(URL)
+        .then(res => res.json())
+        .then(data => {setPersonalData(data); console.log(data)})
+
+        
+    }, [])
+
     /*
         Datos del jugador llegan por props, la idea mas adelante es guardar datos de login en cookie, quizas ni las necesite recibir
         Estadisticas del jugador se hace consulta a back    
@@ -16,9 +34,9 @@ const JugadorEstadisticasToolbar = ({ nombreJugador = "Facundo", fechaNac="1998-
                 <img alt="Imagen Jugador" src={soccerPlayer}/>
             </div>
             <div className="jugToolbar-jugData">
-                <label><b>Nombre:</b> { nombreJugador }</label>
-                <label><b>Fecha Nacimiento:</b> { fechaNac }</label>
-                <label><b>Edad:</b> {getEdadActual(fechaNac)}</label>
+                <label><b>Nombre:</b> { personalData.nombre + " " + personalData.apellido }</label>
+                <label><b>Fecha Nacimiento:</b> { personalData.fechaNacimiento }</label>
+                <label><b>Edad:</b> {getEdadActual(personalData.fechaNacimiento)}</label>
             </div>
             <div className="jugToolbar-jugEstadisticas">
                 <label><b>Goles:</b> {Goles}</label>
